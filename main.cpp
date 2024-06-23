fa61be759e5f3d1e7bd54f4274483dc9e76871bc
caspgin <31950557+caspgin@users.noreply.github.com>
2024-06-23 15:40:58 -0700

    Trying to create a triangle. Created a Data array for a triangle in NDC. Created vertex buffer object and fragment buffer object. Created simple vertex and frag shaders. Created Shader program and attached the shaders. Deleted the shader objects.

--- main.cpp
+++ main.cpp
@@ -3,21 +3,33 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+void framebuffer_size_callback(GLFWwindow * window, int width, int height){
 	glViewport(0,0,width,height);
 }
 
-void processInput(GLFWwindow* window){
+void processInput(GLFWwindow * window){
 	
 	if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){
 		glfwSetWindowShouldClose(window,true);
 	}
 }
 
+const char * vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 aPos;\n"
+	"void main()\n"
+	"{\n"
+	"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+	"}\0";
+
+const char * fragmentShaderSource = "#version 330 core\n"
+	"out vec4 FragColor;\n"
+	"void main()\n"
+	"{\n"
+	"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\0";
+
 int main() {
 
-	std::cout << "Program Started" << std::endl;
-    
 	// Initialize GLFW
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW" << std::endl;
@@ -30,7 +42,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create a windowed mode window and its OpenGL context
-    GLFWwindow* window = glfwCreateWindow(800, 600, "PixelSmith", NULL, NULL);
+    GLFWwindow * window = glfwCreateWindow(800, 600, "PixelSmith", NULL, NULL);
     
 	if (!window) {
         std::cerr << "Failed to create GLFW window" << std::endl;
@@ -46,11 +58,76 @@ int main() {
 		return -1;
 	}
 	
-
     // Set the viewport size and the callback function for window resizing
     glViewport(0, 0, 800, 600);
-
 	glfwSetFramebufferSizeCallback(window,framebuffer_size_callback);
+
+	//Vertex Data
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	//Generate a  buffer object
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	
+	//Bind the buffer object 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	
+	//Upload vertex data to buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//Create a vertex Shader Object
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+	//Attach the shader to the object and compile
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	//Check for successful compilation or failure. Store errors in infoLog
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+	if(!success){
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	//create a fragment Shader Object
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	
+	if(!success){
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	
+	//create a shader program object
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, FragmentShader);
+	glLinkProgram(shaderProgram);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if(!success){
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+	//Activate Program 
+	glUseProgram(shaderProgram);
+	//Delete Shader objects
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
     // Render loop
     while (!glfwWindowShouldClose(window)) {
