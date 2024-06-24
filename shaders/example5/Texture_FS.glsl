02be4f613c17eabf4746e60473b5a611bb913970
caspgin <31950557+caspgin@users.noreply.github.com>
2024-06-24 15:48:35 -0700

    More Shader playing

--- shaders/example5/Texture_FS.glsl
+++ shaders/example5/Texture_FS.glsl
@@ -8,5 +8,6 @@ uniform sampler2D texture2;
 out vec4 FragColor;
 
 void main(){
-	FragColor = mix(texture(texture1, texCords), texture(texture2, vec2(1-texCords.x,texCords.y)), 0.2);
+	vec2 newtex = texCords - vec2(0.5f,0.5f);
+	FragColor = mix(texture(texture1, texCords), texture(texture2, newtex), 0.2);
 }
