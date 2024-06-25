3d391212cdb34a424cc47830f9c626c89f1eb74d
caspgin <31950557+caspgin@users.noreply.github.com>
2024-06-24 21:49:19 -0700

    Added another rectagle using same shader but giving different matrix to the uniform. This time it translates to top left and scales using sin.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 
+#include "glm/trigonometric.hpp"
+#include <cmath>
 #include <glm/ext/matrix_transform.hpp>
 #include <glad/glad.h>
 #include <stb_image.h>
@@ -83,6 +85,8 @@ int main() {
 	fs::path vertexPath = shaderDir / "example5/Texture_VS.glsl";
 	fs::path fragmentPath = shaderDir / "example5/Texture_FS.glsl";
 	Shader shaderProgram(vertexPath, fragmentPath);
+	Shader shader2(shaderDir / "example8/Texture_VS.glsl", shaderDir / "example8/Texture_FS.glsl");
+
 	//Texture generation
 	//------------------------------------------------------------------------------------------------
 	unsigned int texture[2];
@@ -94,6 +98,10 @@ int main() {
 	shaderProgram.use();
 	shaderProgram.setInt("texture1", 0);
 	shaderProgram.setInt("texture2", 1);
+
+	shader2.use();
+	shader2.setInt("texture1", 0);
+	shader2.setInt("texture2", 1);
 
 
 	//Vertex Data, Buffers and attribute linking
@@ -112,19 +120,42 @@ int main() {
 
 
 	//Generate a vertex buffer object and vertex array object
-	unsigned int VBO, VAO, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
+	unsigned int VBO[2], VAO[2], EBO[2];
+	glGenVertexArrays(2, VAO);
+	glGenBuffers(2, VBO);
+	glGenBuffers(2, EBO);
 	
 	//Bind the vertex arrray object
-	glBindVertexArray(VAO);
+	glBindVertexArray(VAO[0]);
 
 	//Bind the buffer object 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*) (3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) (6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+	//Since glVertexAttribPointer bounds the VBO to VAO we can unbind the buffer
+	glBindBuffer(GL_ARRAY_BUFFER,0);
+	glBindVertexArray(0);
+	
+	//Bind the vertex arrray object
+	glBindVertexArray(VAO[1]);
+
+	//Bind the buffer object 
+	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	  
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -167,15 +198,22 @@ int main() {
 		transform = glm::translate(transform, glm::vec3(0.5f,-0.5f,0.0f));
 		transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f,0.0f,1.0f));
 
-
 		shaderProgram.use();
 		shaderProgram.setMat4("transform", transform);
 
-		glBindVertexArray(VAO);
+		glBindVertexArray(VAO[0]);
 		/* glDrawArrays(GL_TRIANGLES, 0, 3); */
 		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glBindVertexArray(0);
-
+		
+		
+		glm::mat4 trans = glm::mat4(1.0f);
+		trans = glm::translate(trans, glm::vec3(-0.5f,0.5f, 0.0f));
+		trans = glm::scale(trans, glm::vec3(-abs(sin((float)glfwGetTime())*0.5),-abs(sin((float)glfwGetTime())*0.5),1.0f) - glm::vec3(0.05f,0.05f,0.0f));
+		shaderProgram.setMat4("transform", trans);		
+		glBindVertexArray(VAO[1]);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
         // Swap front and back buffers and poll events
 		// ---------------------------------------------
         glfwSwapBuffers(window);
@@ -184,9 +222,9 @@ int main() {
 
     // Clean up and exit
 	// -----------------------------------------------------------------------------------
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
-	glDeleteBuffers(1, &EBO);
+	glDeleteVertexArrays(2, VAO);
+	glDeleteBuffers(2, VBO);
+	glDeleteBuffers(2, EBO);
 	shaderProgram.deleteProgram();
     glfwTerminate();
 
---
+++ shaders/example8/Texture_VS.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aCords;
+
+out vec3 vertexColor;
+out vec2 texCords;
+uniform mat4 transform;
+
+void main(){
+	gl_Position = transform * vec4(aPos, 1.0f);
+	vertexColor = aColor;
+	texCords = aCords;
+}
+
